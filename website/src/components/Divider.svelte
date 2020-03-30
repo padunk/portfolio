13772f34739a5094546b2444f4b726b37c40e543
Padunk <abraham.agel@gmail.com>
2020-03-30 17:21:24 +0700

    create divider component (vertical space)

---
+++ website/src/components/Divider.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let width, color;
+</script>
+
+<style>
+  .divider {
+    height: 100%;
+  }
+</style>
+
+<div
+  class="divider"
+  style="width: {width}px; background-color: {color ? color : ''}" />
