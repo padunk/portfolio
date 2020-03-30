e22a4dc570ec22c52cfbeb673164dc2543e61bb1
Padunk <abraham.agel@gmail.com>
2020-03-30 17:22:03 +0700

    create spacer comp (horizontal space)

---
+++ website/src/components/Spacer.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let height, color;
+</script>
+
+<style>
+  .spacer {
+    width: 100%;
+  }
+</style>
+
+<div
+  class="spacer"
+  style="height: {height}px; background-color: {color ? color : ''}" />
