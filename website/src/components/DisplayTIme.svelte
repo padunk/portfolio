2521ad11b7d1d39d99d6e743583d319ba01e55f1
Padunk <abraham.agel@gmail.com>
2020-03-30 17:22:23 +0700

    create new Time component

---
+++ website/src/components/DisplayTIme.svelte
@@ -0,0 +1,27 @@
+<script>
+  //   export let timeToShow;
+
+  let d = new Date();
+
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
+  let show = new Intl.DateTimeFormat("default", options).format(d);
+  // Monday, March 30, 2020
+</script>
+
+<style>
+  .date {
+    display: block;
+    text-align: right;
+    font-size: 2rem;
+    padding-right: 4rem;
+    padding-top: 2rem;
+  }
+</style>
+
+<time class="date mb-sm">{show}</time>
